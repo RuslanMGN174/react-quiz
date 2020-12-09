4c5a62ab1f28b2d1ffeae788cf92475012c8865a
Vegas <rusland174@gmail.com>
2020-12-09 16:55:54 +0500

    react-quiz answer click handler added

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import classes from './ActiveQuiz.module.css'
+import AnswersList from './AnswersList/AnswersList'
 
 const ActiveQuiz = props => (
   <div className={classes.ActiveQuiz}>
     <p className={classes.Question}>
       <span>
-        <strong>2.</strong>&nbsp;
-        Как дела?
+        <strong>{ props.questionNum }</strong>&nbsp;
+        { props.question }
       </span>
       <small>2 из 12</small>
     </p>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-    </ul>
+    <AnswersList
+      answers={props.answers}
+      onAnswerClick={props.onAnswerClick}
+    />
   </div>
 )
 
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -2,18 +2,40 @@ import React, {Component} from 'react'
 import classes from './Quiz.module.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
+let questionNumber = 0
+
 class Quiz extends Component{
   state = {
-    quiz: []
+    quiz: [
+      {
+        questionNum: questionNumber++,
+        question: 'Какого цвета небо?',
+        rightAnswerId: 2,
+        answers: [
+          { text: 'Черный', id: 1 },
+          { text: 'Синий', id: 2 },
+          { text: 'Красный', id: 3 },
+          { text: 'Зелёный', id: 4 }
+        ]
+      }
+    ]
   }
 
+  onAnswerClickHandler = answerId => {
+    console.log(`Answer ID: ${answerId}`)
+  }
 
   render () {
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
-          <h1>Quiz</h1>
-          <ActiveQuiz />
+          <h1>Ответьте на все вопросы</h1>
+          <ActiveQuiz
+            questionNum={this.state.quiz[0].questionNum}
+            question={this.state.quiz[0].question}
+            answers={this.state.quiz[0].answers}
+            onAnswerClick={this.onAnswerClickHandler}
+          />
         </div>
       </div>
     )
